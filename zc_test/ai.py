c9f230b3f30957b6072ffa4e22cf8aec5165b422
“zhuchen” <zhuchen@hortorgames.com>
2023-05-03 22:26:19 +0800

    file

--- zc_test/ai.py
+++ zc_test/ai.py
@@ -1,47 +1,56 @@
-import pandas as pd
-# from text2vec.models import FastText
-from text2vec.vectorizers import Vectorizer
+import csv
 import numpy as np
-from annoy import AnnoyIndex
+from text2vec import Text2Vec
+import faiss
 
-# 加载 text2vec-large-chinese 模型
-model_path = "/home/dev/text2vec-large-chinese"
-# model = FastText.load_fasttext_format(model_path)
+# 设定向量维度
+VECTOR_DIM = 300
 
-# 加载问答数据
-data = pd.read_csv("/home/dev/langchain-ChatGLM/dtl.csv")
+# 读取csv问答文件的问题和答案
+questions = []
+answers = []
+with open('qa_data.csv', 'r', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    for i, row in enumerate(reader):
+        # 跳过标题行
+        if i == 0:
+            continue
+        # 将问题和答案加入列表
+        questions.append(row[0])
+        answers.append(row[1])
+
+# 初始化text2vec模型
+model = Text2Vec('text2vec-large-chinese')
 
 # 将问题和答案转换为向量
-vectorizer = Vectorizer.load(model_path)
-vectors = vectorizer.transform(data["question"] + data["answer"])
-
-# 将向量保存到 numpy 数组中
-vectors_array = np.vstack(list(vectors))
+vectors_list = []
+for text in (questions + answers):
+    vector = model.vectorize(text, size=VECTOR_DIM)
+    vectors_list.append(vector)
+vectors = np.array(vectors_list)
 
 # 建立向量索引
-index = AnnoyIndex(vectors_array.shape[1], "angular")
-for i, vector in enumerate(vectors_array):
-    index.add_item(i, vector)
-index.build(10)
+index = faiss.IndexFlatIP(VECTOR_DIM)
+index.add(vectors)
 
+# 匹配相似度最高的答案
+query = '我想了解一下定期存款的利息'
+query_vector = model.vectorize(query, size=VECTOR_DIM)
+_, similar_ids = index.search(np.array([query_vector]), k=1)
+answer = answers[similar_ids[0][0]]
 
-# 定义一个函数，根据问题匹配相似度最高的答案
-def find_answer(question):
-    # 将输入的问题转换为向量
-    question_vector = vectorizer.transform([question])[0]
-
-    # 使用 annoy 索引查找相似的向量
-    ids, distances = index.get_nns_by_vector(question_vector, 3, include_distances=True)
-
-    # 打印相似度大于 0 的前三个问题和答案，以及对应的相似度
-    for id, distance in zip(ids, distances):
-        if distance > 0:
-            print("Question: ", data.iloc[id]["question"])
-            print("Answer: ", data.iloc[id]["answer"])
-            print("Similarity: ", 1 - distance)
-            print()
-
-
-while True:
-    query = input("Q：")
-    find_answer(query)
+# 输出相似度大于0的前三个问题和答案，以及对应的相似度
+_, similar_ids = index.search(vectors, k=4)
+print('相似度大于0的前三个问题和答案：')
+for i, similarity_list in enumerate(similar_ids):
+    similar_questions = ', '.join([questions[s] for s in similarity_list[1:] if s != i])
+    if similar_questions:
+        print('\n原问题：', questions[i])
+        print('相似问题：', similar_questions)
+        print('原回答：', answers[i])
+        for j, s in enumerate(similarity_list[1:]):
+            if s != i:
+                print(f'相似问题{j+1}：{questions[s]}')
+                print(f'相似回答{j+1}：{answers[s]}')
+                print(f'相似度{j+1}：{index.search(np.array([vectors[s]]), k=1)[0][0]}')
+    print('----------------------------------------')
