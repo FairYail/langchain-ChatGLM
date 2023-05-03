382942752e6790170215854e58fce8f15f3b0a7b
“zhuchen” <zhuchen@hortorgames.com>
2023-05-03 22:27:21 +0800

    file

--- zc_test/ai.py
+++ zc_test/ai.py
@@ -9,7 +9,7 @@ VECTOR_DIM = 300
 # 读取csv问答文件的问题和答案
 questions = []
 answers = []
-with open('qa_data.csv', 'r', encoding='utf-8') as f:
+with open('/home/dev/langchain-ChatGLM/zc.csv', 'r', encoding='utf-8') as f:
     reader = csv.reader(f)
     for i, row in enumerate(reader):
         # 跳过标题行
@@ -34,23 +34,24 @@ index = faiss.IndexFlatIP(VECTOR_DIM)
 index.add(vectors)
 
 # 匹配相似度最高的答案
-query = '我想了解一下定期存款的利息'
-query_vector = model.vectorize(query, size=VECTOR_DIM)
-_, similar_ids = index.search(np.array([query_vector]), k=1)
-answer = answers[similar_ids[0][0]]
+while True:
+    query = input("Q：")
+    query_vector = model.vectorize(query, size=VECTOR_DIM)
+    _, similar_ids = index.search(np.array([query_vector]), k=1)
+    answer = answers[similar_ids[0][0]]
 
-# 输出相似度大于0的前三个问题和答案，以及对应的相似度
-_, similar_ids = index.search(vectors, k=4)
-print('相似度大于0的前三个问题和答案：')
-for i, similarity_list in enumerate(similar_ids):
-    similar_questions = ', '.join([questions[s] for s in similarity_list[1:] if s != i])
-    if similar_questions:
-        print('\n原问题：', questions[i])
-        print('相似问题：', similar_questions)
-        print('原回答：', answers[i])
-        for j, s in enumerate(similarity_list[1:]):
-            if s != i:
-                print(f'相似问题{j+1}：{questions[s]}')
-                print(f'相似回答{j+1}：{answers[s]}')
-                print(f'相似度{j+1}：{index.search(np.array([vectors[s]]), k=1)[0][0]}')
-    print('----------------------------------------')
+    # 输出相似度大于0的前三个问题和答案，以及对应的相似度
+    _, similar_ids = index.search(vectors, k=4)
+    print('相似度大于0的前三个问题和答案：')
+    for i, similarity_list in enumerate(similar_ids):
+        similar_questions = ', '.join([questions[s] for s in similarity_list[1:] if s != i])
+        if similar_questions:
+            print('\n原问题：', questions[i])
+            print('相似问题：', similar_questions)
+            print('原回答：', answers[i])
+            for j, s in enumerate(similarity_list[1:]):
+                if s != i:
+                    print(f'相似问题{j+1}：{questions[s]}')
+                    print(f'相似回答{j+1}：{answers[s]}')
+                    print(f'相似度{j+1}：{index.search(np.array([vectors[s]]), k=1)[0][0]}')
+        print('----------------------------------------')
